692d206d19266fcf7299c7153752f8a07cd3e4ff
probablyduncan <duncanpetrie1@gmail.com>
2024-12-22 21:50:17 -0600

    integrations and finally got my own package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import clerk from "@clerk/astro";
+import netlify from "@astrojs/netlify";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [clerk(), tailwind()],
+
+    env: {
+        schema: {
+            PUBLIC_CLERK_PUBLISHABLE_KEY: envField.string({
+                context: "client",
+                access: "public",
+            }),
+            CLERK_SECRET_KEY: envField.string({
+                context: "server",
+                access: "secret",
+            }),
+        }
+    },
+
+    adapter: netlify(),
+    output: "static",
+
+});
